af5a018ff08bc72d734ceee9a106b721b6122211
Aqll <aqilyearoo@gmail.com>
2021-06-12 15:13:33 +0400

    Added more details when starting the game

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+alert("This is a rock-paper-scissors game with 3 rounds.");
+
 //Array of moves:
 let moves = ['rock', 'paper', 'scissors'];
 
@@ -30,8 +32,9 @@ function play(playerSelection, computerSelection){
 function game(){
     let playerScore = 0, computerScore = 0;
 
-    for (let i = 0; i < 5; i++){
-        let playerSelection = window.prompt("Please enter rock, paper, or scissors");
+    for (let i = 0; i < 3; i++){
+        let z = i+1;
+        let playerSelection = window.prompt("Round "+ z +": Please enter rock, paper, or scissors");
         const computerSelection = computer();
         let winner = play(playerSelection, computerSelection);
         if (winner === 'Computer wins!') computerScore += 1;
